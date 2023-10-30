daa0d0d6bd8133b209c736e75144dd104dbf737b
Nimi-2 <emmadra20@gmail.com>
2023-10-30 16:17:18 +0100

    poprawienie styli

--- client/screens/AreaScreen.jsx
+++ client/screens/AreaScreen.jsx
@@ -91,10 +91,11 @@ export default function AreaScreen() {
         )}
       {!showPicker &&(
         <Pressable
+          style={[styles.box,{width:"90%"}]} 
           onPress={toggleDatePicker}
         >
           <TextInput 
-            style={[styles.textinput,{color:"black"}]} 
+            style={[styles.box,{width:"90%",color:"black"}]} 
             onPress={toggleDatePicker}
             onChangeText={setDateArea}
             value={dateReady}
@@ -105,20 +106,20 @@ export default function AreaScreen() {
         </Pressable>
       )}
       </View>
-        <View style={{ margin: 10 }}>
-          <Text>Cykliczność:</Text>
+        <View style={{ margin: 10,  }}>
+        <Text style={[styles.title,{marginRight:"60%"}]}>Cykliczność:</Text>
           {opcje.map((opcja) => (
             <TouchableOpacity
               key={opcja.id}
               onPress={() => selectOptions(opcja.id)}
-              style={{ flexDirection: "row", alignItems: "center" }}
+              style={{ flexDirection: "row", alignItems: "center",marginRight:"60%", margin:5 }}
             >
               {option === opcja.id ? (
                 <AntDesignIcon name="checkcircleo" size={20} color="blue" />
               ) : (
                 <EntypoIcon name="circle" size={20} color="black" />
               )}
-              <Text>{opcja.label}</Text>
+              <Text >{opcja.label}</Text>
             </TouchableOpacity>
           ))}
         </View>
@@ -126,7 +127,7 @@ export default function AreaScreen() {
             style={styles.button}
             onPress={() => navigation.navigate("AreaSelect")}
           >
-            <Text>Zapisz</Text>
+            <Text style={{color:"white"}}>Zapisz</Text>
         </TouchableOpacity>
       </View>
     );
@@ -149,7 +150,7 @@ export default function AreaScreen() {
     const [selectedValue, setSelectedValue] = useState("Podopieczny/grupa");
 
     return (
-      <View style={{ alignItems: "center", height: "100%" , paddingTop: 25 }}>
+      <View style={{ alignItems: "center", height: "100%" , paddingTop: 25, flexDirection:"column" }}>
         <View style={styles.index}>
           <TouchableOpacity>
             <AntDesignIcon
@@ -164,16 +165,18 @@ export default function AreaScreen() {
             <AntDesignIcon name="infocirlceo" size={20} color="black" />
           </TouchableOpacity>
         </View>
-        <View style={styles.container}>
+        <View style={[styles.box, { alignItems: "center", width: "90%",height:"10%"   }]}>
           <TextInput
-            style={styles.textinput}
+            
             placeholder="Nazwa obszaru"
           ></TextInput>
+          </View>
+          <View View style={[styles.box, { alignItems: "center",width: "90%" }]}>
           <Picker
             style={{
               backgroundColor: "rgb(219, 219, 219)",
               height: 30,
-              width: 150,
+              width: "100%",
               borderRadius: 20,
             }}
             selectedValue={selectedValue}
@@ -181,15 +184,17 @@ export default function AreaScreen() {
               setSelectedValue(itemValue)
             }
           >
-            <Picker.Item label="Podopieczny/grupa" value="Podopieczny/grupa" />
+            <Picker.Item label="Podopieczny/grupa" value="Podopieczny/grupa"/>
             <Picker.Item label="Anna Nowak" value="Anna Nowak" />
             <Picker.Item label="Jan Kowalski" value="Jan Kowalski" />
           </Picker>
-          <TouchableOpacity onPress={{}}>
+          </View>
+          <View style={ { alignItems: "center",width:"90%" }}>
+          <TouchableOpacity onPress={{}}  style={{width:"90%"}}>
             <Image
               style={{
-                height: 100,
-                width: 100,
+                height: 350,
+                width: 350,
                 borderRadius: 8,
                 marginTop: 8,
                 marginBottom: 20,
@@ -197,11 +202,13 @@ export default function AreaScreen() {
               source={require("../assets/map.jpg")}
             />
           </TouchableOpacity>
+          </View>
+          <View>
           <TouchableOpacity
             style={styles.button}
             onPress={() => navigation.navigate("AreaTime")}
           >
-            <Text>Dalej</Text>
+            <Text style={{color:"white"}}>Dalej</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -238,10 +245,12 @@ export default function AreaScreen() {
           </Text>
           <EviliconsIcon name="user" size={50} color="black" style={{}} />
         </View>
+        <View style={{width:"60%", height:"60%"}}>
         <Image
           style={{ width: 175, height: 150, borderRadius: 5 }}
           source={require("../assets/map.jpg")}
         />
+        </View>
       </View>
     );
 
