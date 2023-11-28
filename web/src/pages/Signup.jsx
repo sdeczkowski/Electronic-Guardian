cdb3e3ae93c4fbbf708647ef8129af73394c700c
sdeczkowski <106768651+sdeczkowski@users.noreply.github.com>
2023-11-28 22:19:40 +0100

    Ed29 (#57)
    * profile and chat
    
    * pro x log = prolog
    
    * logowanie git / poprawic przyciski profile
    
    ---------
    
    Co-authored-by: Nimi-2 <emmadra20@gmail.com>

--- web/src/pages/Signup.jsx
+++ web/src/pages/Signup.jsx
@@ -1,39 +1,63 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import uni from '../assets/uni.png';
+import "../styles/style.css";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 const Signup = () => {
   const [data, setData] = useState({
-    name: "",
     email: "",
     password: "",
-  })
-  const [error, setError] = useState("")
-  const navigate = useNavigate()
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handleChange = ({ currentTarget: input }) => {
-    setData({ ...data, [input.name]: input.value })
-  }
+    setData({ ...data, [input.name]: input.value });
+  };
 
   const handleSubmit = async () => {
-    
+    // Your submission logic goes here
   };
+
   return (
     <div className="xd">
-        <img src={uni} alt='logo' className="xd2"/>
-        <div className="xd3">
-          <h5 className="title">Logowanie</h5>
-          <br />
-          <Form.Control  type="text" placeholder="Large text" className="input"/>
-          <br />
-          <Form.Control type="text" placeholder="Normal text" className="input"/>
-          <br />
-          <button className="button1">Zaloguj się</button>
+      <img src={uni} alt='logo' className="xd2" />
+      <div className="xd3">
+        <h5 className="title">Logowanie</h5>
+        <br />
+        <Form.Control
+          type="text"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          className="input"
+          style={{width:"40%", borderRadius:"20vh"}}
+        />
+        <br />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+          className="input input-container"
+          style={{width:"40%", borderRadius:"20vh"}}
+        />
+        <br />
+        <div className="xd2 input input-container">
+          <div>
+            <button className="button1" onClick={handleSubmit}>
+              Zaloguj się
+            </button>
+          </div>
+          <Link to="/register" className="input input-container">
+            Nie masz konta?
+          </Link>
         </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Signup;
