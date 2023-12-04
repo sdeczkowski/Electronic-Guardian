d590b41425cf54dd1398136a6cf176c497d5b07c
sdeczkowski <szymondeczkowski@gmail.com>
2023-12-04 22:53:59 +0100

    co

--- web/src/pages/Profile.jsx
+++ web/src/pages/Profile.jsx
@@ -27,84 +27,16 @@ const Profile = () => {
   const [errEmail, setErrEmail] = useState(false);
   const [errNewEmail, setErrNewEmail] = useState(false);
   const [errRepeatEmail, setErrRepeatEmail] = useState(false);
-  const [email, setEmail] = useState("");
-  const [newemail, setNewEmail] = useState("");
-  const [repeatemail, setRepeatEmail] = useState("");
-  const [check, setCheck] = useState(null);
-  const [errEmail, setErrEmail] = useState(false);
-  const [errNewEmail, setErrNewEmail] = useState(false);
-  const [errRepeatEmail, setErrRepeatEmail] = useState(false);
 
   const toggleDeactivate = () => setShowDeactivate(!showDeactivate);
   const toggleEmail = () => setShowEmail(!showEmail);
   const togglePass = () => setShowPass(!showPass);
-
 
   const handleChange = (checked) => {
     setChecked(checked);
   };
   const handleChange2 = (checked2) => {
     setChecked2(checked2);
-  };
-
-  const handleEmail = async () => {
-    setCheck(null);
-    setErrRepeatEmail(false);
-    setErrNewEmail(false);
-    setErrEmail(false);
-    const emailRegex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
-    if (email === "") {
-      setErrEmail(true);
-      setCheck("Wpisz email!");
-    } else if (newemail === "") {
-      setErrNewEmail(true);
-      setCheck("Wpisz nowy email!");
-    } else if (repeatemail === "") {
-      setErrRepeatEmail(true);
-      setCheck("Wpisz ponownie nowy email!");
-    } else if (repeatemail !== newemail) {
-      setErrNewEmail(true);
-      setErrRepeatEmail(true);
-      setCheck("Emaile nie są identyczne!");
-    } else if (!emailRegex.test(newemail)) {
-      setErrNewEmail(true);
-      setErrRepeatEmail(true);
-      setCheck("Niepoprawny email!");
-    } else if (email === newemail) {
-      setErrEmail(true);
-      setErrNewEmail(true);
-      setErrRepeatEmail(true);
-      setCheck("Nowy i stary email są identyczne!");
-    } else {
-      const _id = localStorage.getItem("_id");
-      try {
-        const url = "http://localhost:3001/api/user/updatemail";
-        await axios.post(url, {
-          _id: _id,
-          email: email,
-          new_email: newemail,
-        });
-        toggleEmail();
-      } catch (error) {
-        if (error.response && error.response.status >= 400 && error.response.status <= 500) {
-          if (error.response.status === 401) {
-            if (error.response.data.message === "Inny użytkownik przypisał już ten email") {
-              setErrNewEmail(true);
-              setErrRepeatEmail(true);
-            }
-            if (error.response.data.message === "Błędny email") {
-              setErrEmail(true);
-            }
-            setCheck(error.response.data.message);
-          }
-        }
-      }
-    }
-  };
-
-  const LogOut = () => {
-    localStorage.clear();
-    window.location.replace("/");
   };
 
   const handleEmail = async () => {
@@ -175,33 +107,8 @@ const Profile = () => {
             className="rounded-circle roundbutton bg-light"
             style={{ borderWidth: 1, borderColor: "#979797", backgroundColor: "white", width: "80px", height: "80px" }}>
             <img src={uni} alt="logo" style={{ width: "50px", height: "50px" }} />
-        <Nav style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <Button
-            className="rounded-circle roundbutton bg-light"
-            style={{ borderWidth: 1, borderColor: "#979797", backgroundColor: "white", width: "80px", height: "80px" }}>
-            <img src={uni} alt="logo" style={{ width: "50px", height: "50px" }} />
           </Button>
           <div className="sidebar">
-            <Nav.Item>
-              <Nav.Link href="/profile">
-                <FaRegCircleUser style={{ color: "#2699c7", fontSize: "4vh" }} />
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-1" href="/">
-                <FaMap style={{ color: "#979797", fontSize: "4vh" }} />
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-2" href="/list">
-                <FaLocationDot style={{ color: "#979797", fontSize: "4vh" }} />
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="disabled" href="/chat">
-                <FaMessage style={{ color: "#979797", fontSize: "4vh" }} />
-              </Nav.Link>
-            </Nav.Item>
             <Nav.Item>
               <Nav.Link href="/profile">
                 <FaRegCircleUser style={{ color: "#2699c7", fontSize: "4vh" }} />
@@ -308,40 +215,7 @@ const Profile = () => {
                   className="input"
                   style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
                 />
-            <Button variant="link" color="gray" onClick={togglePass}>
-              <BsChevronRight size={20} className="react-switch3" />
-            </Button>
-            <Modal show={showPass} onHide={togglePass}>
-              <Modal.Header closeButton></Modal.Header>
-              <Modal.Title style={{ display: "flex", justifyContent: "center" }}>Zmiana hasła</Modal.Title>
-              <Modal.Body
-                style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
-                <Form.Control
-                  type="text"
-                  placeholder="Stare hasło"
-                  name="email"
-                  onChange={handleChange}
-                  className="input"
-                  style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
-                />
-                <Form.Control
-                  type="text"
-                  placeholder="Nowy hasło"
-                  name="email"
-                  onChange={handleChange}
-                  className="input"
-                  style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
-                />
-                <Form.Control
-                  type="text"
-                  placeholder="Powtórz nowe hasło"
-                  name="email"
-                  onChange={handleChange}
-                  className="input"
-                  style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
-                />
               </Modal.Body>
-              <Modal.Footer style={{ display: "flex", alignContent: "space-between", justifyContent: "center" }}>
               <Modal.Footer style={{ display: "flex", alignContent: "space-between", justifyContent: "center" }}>
                 <button className="button" style={{ backgroundColor: "deepskyblue" }} onClick={togglePass}>
                   Zmień
@@ -383,42 +257,7 @@ const Profile = () => {
                   className="input"
                   style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
                 />
-            <div>Zmień email</div>
-            <Button variant="link" onClick={toggleEmail}>
-              <BsChevronRight size={20} className="react-switch2" />
-            </Button>
-            <Modal show={showEmail} onHide={toggleEmail}>
-              <Modal.Header closeButton></Modal.Header>
-              <Modal.Title style={{ display: "flex", justifyContent: "center" }}>Zmiana adresu email</Modal.Title>
-              <Modal.Body
-                style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
-                <Form.Control
-                  type="text"
-                  placeholder="Stary e-mail"
-                  name="email"
-                  onChange={handleChange}
-                  className="input"
-                  style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
-                />
-                <Form.Control
-                  type="text"
-                  placeholder="Nowy e-mail"
-                  name="email"
-                  onChange={handleChange}
-                  className="input"
-                  style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
-                />
-                <Form.Control
-                  type="text"
-                  placeholder="Powtórz nowy e-mail"
-                  name="email"
-                  onChange={handleChange}
-                  className="input"
-                  style={{ width: "60%", borderRadius: "20vh", marginTop: "10px" }}
-                />
               </Modal.Body>
-              <Modal.Footer style={{ display: "flex", alignContent: "space-between", justifyContent: "center" }}>
-                <button className="button" style={{ backgroundColor: "deepskyblue" }} onClick={handleEmail}>
               <Modal.Footer style={{ display: "flex", alignContent: "space-between", justifyContent: "center" }}>
                 <button className="button" style={{ backgroundColor: "deepskyblue" }} onClick={handleEmail}>
                   Zmień
@@ -430,24 +269,11 @@ const Profile = () => {
             <button className="button" onClick={toggleDeactivate}>
               Dezaktywuj konto
             </button>
-            <button className="button" onClick={toggleDeactivate}>
-              Dezaktywuj konto
-            </button>
             <Modal show={showDeactivate} onHide={toggleDeactivate}>
-              <Modal.Header style={{ display: "flex", flexDirection: "column" }}>
-                <HiExclamationTriangle size={150} color="red" />
               <Modal.Header style={{ display: "flex", flexDirection: "column" }}>
                 <HiExclamationTriangle size={150} color="red" />
                 <Modal.Title>Dezaktywacja konta</Modal.Title>
               </Modal.Header>
-              <Modal.Body style={{ display: "flex", justifyContent: "center" }}>
-                Czy na pewno chcesz dezaktywować konto?
-              </Modal.Body>
-              <Modal.Footer style={{ display: "flex", alignContent: "space-between", justifyContent: "center" }}>
-                <Button
-                  variant="link"
-                  onClick={toggleDeactivate}
-                  style={{ display: "flex", justifyContent: "center", textDecoration: "none", color: "black" }}>
               <Modal.Body style={{ display: "flex", justifyContent: "center" }}>
                 Czy na pewno chcesz dezaktywować konto?
               </Modal.Body>
@@ -462,18 +288,11 @@ const Profile = () => {
                   variant="link"
                   onClick={toggleDeactivate}
                   style={{ display: "flex", justifyContent: "center", textDecoration: "none", color: "black" }}>
-                <Button
-                  variant="link"
-                  onClick={toggleDeactivate}
-                  style={{ display: "flex", justifyContent: "center", textDecoration: "none", color: "black" }}>
                   Tak
                 </Button>
               </Modal.Footer>
             </Modal>
             <button className="button" onClick={LogOut}>Wyloguj się</button>
-            <button className="button" onClick={LogOut}>
-              Wyloguj się
-            </button>
           </div>
         </div>
       </div>
